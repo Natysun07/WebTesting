500526a0568426179eca7cdbb3c45995aeb47ea4
Natalia <natysun07@rambler.ru>
2023-06-24 22:15:47 +0300

    change appveyor.yml file, change test method, add tests

--- src/test/java/ru/netology/web/WebTestingTest.java
+++ src/test/java/ru/netology/web/WebTestingTest.java
@@ -1,41 +1,14 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class WebTestingTest {
-    private static WebDriver driver;
-
-
-    @BeforeEach
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver\\win\\chromedriver.exe");
-    }
-
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
-        driver = null;
-    }
 
     @Test
     public void shouldOpenPage() {
@@ -49,7 +22,8 @@ public class WebTestingTest {
         $(By.cssSelector("[data-test-id=phone] input")).sendKeys("+74952592741");
         $(By.cssSelector(".checkbox__box")).click();
         $(By.cssSelector("button")).click();
-        $(By.cssSelector(".paragraph")).shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(By.cssSelector("[data-test-id='order-success']")).shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        ;
 
     }
 
@@ -65,6 +39,16 @@ public class WebTestingTest {
     }
 
     @Test
+    public void shouldSendWithoutName() {
+        open("http://localhost:9999");
+        $(By.cssSelector("[data-test-id=name] input")).sendKeys("");
+        $(By.cssSelector("[data-test-id=phone] input")).sendKeys("+74952592741");
+        $(By.cssSelector(".checkbox__box")).click();
+        $(By.cssSelector("button")).click();
+        $(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
     public void shouldEnterIncorrectPhone() {
         open("http://localhost:9999");
         $(By.cssSelector("[data-test-id=name] input")).sendKeys("Бакулина Наталья");
@@ -75,11 +59,22 @@ public class WebTestingTest {
     }
 
     @Test
+    public void shouldEnterЦшерщгеPhone() {
+        open("http://localhost:9999");
+        $(By.cssSelector("[data-test-id=name] input")).sendKeys("Бакулина Наталья");
+        $(By.cssSelector("[data-test-id=phone] input")).sendKeys("");
+        $(By.cssSelector(".checkbox__box")).click();
+        $(By.cssSelector("button")).click();
+        $(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
     public void shouldDoNotClickCheckbox() {
         open("http://localhost:9999");
         $(By.cssSelector("[data-test-id=name] input")).sendKeys("Бакулина Наталья");
         $(By.cssSelector("[data-test-id=phone] input")).sendKeys("+74952592741");
-        $(By.cssSelector(".checkbox__text")).shouldHave(Condition.cssValue("color", "rgba(11, 31, 53, 0.95)"));
+        $(By.cssSelector("button")).click();
+        $(By.cssSelector("[data-test-id=agreement].input_invalid")).shouldBe(Condition.visible).shouldHave(Condition.text("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 
 
